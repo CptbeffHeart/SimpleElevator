6b3b63deab77700412a077c78c235a3072278340
CptBeffHeart <cptbeffheart@gmail.com>
2024-04-26 16:08:06 +0200

    feat: create colored elevator block

--- src/main/kotlin/com/expectale/registry/Blocks.kt
+++ src/main/kotlin/com/expectale/registry/Blocks.kt
@@ -21,6 +21,21 @@ object Blocks : BlockRegistry by SimpleElevator.registry {
         SoundGroup.STONE,
         Material.STONE)
     
+    val BLACK_ELEVATOR = block("black_elevator").blockOptions(ELEVATOR_OPTION).register()
+    val BLUE_ELEVATOR = block("blue_elevator").blockOptions(ELEVATOR_OPTION).register()
+    val BROWN_ELEVATOR = block("brown_elevator").blockOptions(ELEVATOR_OPTION).register()
+    val CYAN_ELEVATOR = block("cyan_elevator").blockOptions(ELEVATOR_OPTION).register()
+    val GRAY_ELEVATOR = block("gray_elevator").blockOptions(ELEVATOR_OPTION).register()
+    val GREEN_ELEVATOR = block("green_elevator").blockOptions(ELEVATOR_OPTION).register()
+    val LIGHT_BLUE_ELEVATOR = block("light_blue_elevator").blockOptions(ELEVATOR_OPTION).register()
+    val LIGHT_GRAY_ELEVATOR = block("light_gray_elevator").blockOptions(ELEVATOR_OPTION).register()
+    val LIME_ELEVATOR = block("lime_elevator").blockOptions(ELEVATOR_OPTION).register()
+    val MAGENTA_ELEVATOR = block("magenta_elevator").blockOptions(ELEVATOR_OPTION).register()
+    val ORANGE_ELEVATOR = block("orange_elevator").blockOptions(ELEVATOR_OPTION).register()
+    val PINK_ELEVATOR = block("pink_elevator").blockOptions(ELEVATOR_OPTION).register()
+    val PURPLE_ELEVATOR = block("purple_elevator").blockOptions(ELEVATOR_OPTION).register()
+    val RED_ELEVATOR = block("red_elevator").blockOptions(ELEVATOR_OPTION).register()
     val WHITE_ELEVATOR = block("white_elevator").blockOptions(ELEVATOR_OPTION).register()
+    val YELLOW_ELEVATOR = block("yellow_elevator").blockOptions(ELEVATOR_OPTION).register()
 
 }
