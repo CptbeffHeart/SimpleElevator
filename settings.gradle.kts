a48e4f0f3bd8aba7b66ce7a84b348faa1118c610
CptBeffHeart <cptbeffheart@gmail.com>
2024-04-26 16:12:43 +0200

    update nova version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("xyz.xenondevs.nova:catalog:0.16-RC.1")
+            from("xyz.xenondevs.nova:catalog:0.16")
         }
     }
 }
