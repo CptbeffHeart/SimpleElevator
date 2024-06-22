6195a74fe4314df540b93a72fa88e55ab93267ed
CptBeffHeart <cptbeffheart@gmail.com>
2024-06-22 17:55:47 +0200

    feat: add project distributor to detect new versions

--- src/main/kotlin/com/expectale/SimpleElevator.kt
+++ src/main/kotlin/com/expectale/SimpleElevator.kt
@@ -1,11 +1,14 @@
 package com.expectale
 
 import xyz.xenondevs.nova.addon.Addon
+import xyz.xenondevs.nova.update.ProjectDistributor
 import java.util.logging.Logger
 
 lateinit var LOGGER: Logger
 
 object SimpleElevator : Addon() {
+    
+    override val projectDistributors = listOf(ProjectDistributor.github("CptbeffHeart/SimpleElevator"))
     
     override fun init() {
         LOGGER = logger
