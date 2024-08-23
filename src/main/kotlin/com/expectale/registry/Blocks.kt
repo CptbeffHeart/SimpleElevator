2b7c570bbd0a82a1aaa3659de9b063970e899163
CptBeffHeart <cptbeffheart@gmail.com>
2024-08-23 14:29:17 +0200

    refactor: update to nova 0.17

--- src/main/kotlin/com/expectale/registry/Blocks.kt
+++ src/main/kotlin/com/expectale/registry/Blocks.kt
@@ -5,37 +5,47 @@ import org.bukkit.Material
 import xyz.xenondevs.nova.addon.registry.BlockRegistry
 import xyz.xenondevs.nova.initialize.Init
 import xyz.xenondevs.nova.initialize.InitStage
-import xyz.xenondevs.nova.item.options.BlockOptions
-import xyz.xenondevs.nova.item.tool.VanillaToolCategories
-import xyz.xenondevs.nova.item.tool.VanillaToolTiers
+import xyz.xenondevs.nova.resources.layout.block.BackingStateCategory
+import xyz.xenondevs.nova.world.block.NovaBlock
+import xyz.xenondevs.nova.world.block.behavior.BlockSounds
+import xyz.xenondevs.nova.world.block.behavior.Breakable
 import xyz.xenondevs.nova.world.block.sound.SoundGroup
+import xyz.xenondevs.nova.world.item.tool.VanillaToolCategories
+import xyz.xenondevs.nova.world.item.tool.VanillaToolTiers
 
 @Init(stage = InitStage.PRE_PACK)
 object Blocks : BlockRegistry by SimpleElevator.registry {
     
-    private val ELEVATOR_OPTION = BlockOptions(
+    private val ELEVATOR = Breakable(
         2.0,
         VanillaToolCategories.PICKAXE,
         VanillaToolTiers.STONE,
         true,
-        SoundGroup.STONE,
         Material.STONE)
     
-    val BLACK_ELEVATOR = block("black_elevator").blockOptions(ELEVATOR_OPTION).register()
-    val BLUE_ELEVATOR = block("blue_elevator").blockOptions(ELEVATOR_OPTION).register()
-    val BROWN_ELEVATOR = block("brown_elevator").blockOptions(ELEVATOR_OPTION).register()
-    val CYAN_ELEVATOR = block("cyan_elevator").blockOptions(ELEVATOR_OPTION).register()
-    val GRAY_ELEVATOR = block("gray_elevator").blockOptions(ELEVATOR_OPTION).register()
-    val GREEN_ELEVATOR = block("green_elevator").blockOptions(ELEVATOR_OPTION).register()
-    val LIGHT_BLUE_ELEVATOR = block("light_blue_elevator").blockOptions(ELEVATOR_OPTION).register()
-    val LIGHT_GRAY_ELEVATOR = block("light_gray_elevator").blockOptions(ELEVATOR_OPTION).register()
-    val LIME_ELEVATOR = block("lime_elevator").blockOptions(ELEVATOR_OPTION).register()
-    val MAGENTA_ELEVATOR = block("magenta_elevator").blockOptions(ELEVATOR_OPTION).register()
-    val ORANGE_ELEVATOR = block("orange_elevator").blockOptions(ELEVATOR_OPTION).register()
-    val PINK_ELEVATOR = block("pink_elevator").blockOptions(ELEVATOR_OPTION).register()
-    val PURPLE_ELEVATOR = block("purple_elevator").blockOptions(ELEVATOR_OPTION).register()
-    val RED_ELEVATOR = block("red_elevator").blockOptions(ELEVATOR_OPTION).register()
-    val WHITE_ELEVATOR = block("white_elevator").blockOptions(ELEVATOR_OPTION).register()
-    val YELLOW_ELEVATOR = block("yellow_elevator").blockOptions(ELEVATOR_OPTION).register()
-
+    val BLACK_ELEVATOR = elevatorBlock("black_elevator")
+    val BLUE_ELEVATOR = elevatorBlock("blue_elevator")
+    val BROWN_ELEVATOR = elevatorBlock("brown_elevator")
+    val CYAN_ELEVATOR = elevatorBlock("cyan_elevator")
+    val GRAY_ELEVATOR = elevatorBlock("gray_elevator")
+    val GREEN_ELEVATOR = elevatorBlock("green_elevator")
+    val LIGHT_BLUE_ELEVATOR = elevatorBlock("light_blue_elevator")
+    val LIGHT_GRAY_ELEVATOR = elevatorBlock("light_gray_elevator")
+    val LIME_ELEVATOR = elevatorBlock("lime_elevator")
+    val MAGENTA_ELEVATOR = elevatorBlock("magenta_elevator")
+    val ORANGE_ELEVATOR = elevatorBlock("orange_elevator")
+    val PINK_ELEVATOR = elevatorBlock("pink_elevator")
+    val PURPLE_ELEVATOR = elevatorBlock("purple_elevator")
+    val RED_ELEVATOR = elevatorBlock("red_elevator")
+    val WHITE_ELEVATOR = elevatorBlock("white_elevator")
+    val YELLOW_ELEVATOR = elevatorBlock("yellow_elevator")
+    
+    private fun elevatorBlock(
+        name: String
+    ): NovaBlock = block(name) {
+        behaviors(ELEVATOR, BlockSounds(SoundGroup.STONE))
+        models {
+            stateBacked(BackingStateCategory.MUSHROOM_BLOCK, BackingStateCategory.NOTE_BLOCK)
+        }
+    }
 }
