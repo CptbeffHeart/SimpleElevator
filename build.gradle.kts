6f5855c0bc564fc35c98d9ff67d4c79e523d08e7
CptBeffHeart <cptbeffheart@gmail.com>
2024-08-23 15:19:01 +0200

    chore: Bump version to 3.0.0 (major)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "com.expectale"
-version = "2.0.0"
+version = "3.0.0"
 
 plugins {
     alias(libs.plugins.kotlin)
